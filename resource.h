33048bdc89a64cdb16e567fad86215bb609777db
elexor <bgm2010@gmail.com>
2023-08-31 03:34:27 +1000

    test xg2431 auto strobetune

--- resource.h
+++ resource.h
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 //{{NO_DEPENDENCIES}}
 // Microsoft Visual C++ generated include file.
 // Used by NvQFTswitcher.rc
@@ -14,3 +15,21 @@
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
+=======
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by NvQFTswitcher.rc
+//
+#define IDI_ICON1                       101
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        102
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
+>>>>>>> 7932b6adbd946176f20a1f479d843d071858ca39
